666dc2dd9cc36045dd98135d50376cde27e494cf
Jack Hall <jack.hall@newfireglobal.com>
2025-08-26 19:41:06 -0400

    cleaner version of stock price fetcher, added gitignore and requirements file

--- stock_prices.py
+++ stock_prices.py
@@ -1,21 +1,59 @@
-import yfinance as yf
-import datetime 
+from datetime import date
+from pathlib import Path
+import sys
+from typing import List, Tuple
+
+import yfinance
+import pandas as pd
 
 
-''' List of 5 stocks tickers
-    Meta = META
-    Apple = AAPL
-    Amazon = AMZN
-    Netflix = NFLX
-    Google = GOOGL '''
+def fetch_stocks(symbols: List[str], start_date: date, end_date: date) -> List[pd.DataFrame]:
+    return [
+        yfinance.download(symbol, start=start_date, end=end_date)
+        for symbol in symbols
+    ]
 
-stock_tickers = ["META", "AAPL", "AMZN", "NFLX", "GOOGL"]
 
-for ticker in stock_tickers:
-    tick = yf.Ticker(ticker)
-    print("Company Website :", tick.info['website'])
+def stock_history_path(symbol: str) -> Path:
+    return Path(f"stock_history/{symbol}_history.csv")
 
-def period():
-    start_date = datetime.datetime(2025, 7, 1)
-    end_date = datetime.datetime(2025, 7, 31)
-    
+
+def save_stock(symbol: str, df: pd.DataFrame):
+    df.to_csv(stock_history_path(symbol), index=True)
+
+
+def load_stock(symbol: str) -> pd.DataFrame:
+    return pd.read_csv(stock_history_path(symbol))
+
+
+def parse_dates(dates_arg: str) -> Tuple[date, date]:
+    """Accepts two ISO dates separated by a hyphen.
+    e.g. 20250701-20250801
+    """
+    try:
+        start_str, end_str = dates_arg.split("-")
+    except IndexError:
+        raise RuntimeError("dates must be separated by a '-'")
+
+    try:
+        start = date.fromisoformat(start_str)
+        end = date.fromisoformat(end_str)
+    except Exception:
+        raise RuntimeError("dates must be in ISO format")
+
+    assert start < end, "start date must precede end date"
+    return start, end
+
+
+if __name__ == "__main__":
+    # symbols = ["AAPL", "META", "AMZN", "NFLX", "GOOGL"]
+    # start = date(2025, 7, 1)
+    # end = date(2025, 8, 1)
+
+    start, end = parse_dates(sys.argv[1])
+    symbols = sys.argv[2:]
+
+    stocks = fetch_stocks(symbols, start, end)
+    for symbol, stock in zip(symbols, stocks):
+        save_stock(symbol, stock)
+
